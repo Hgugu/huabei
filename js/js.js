0fd1a2b153121d10537d0d6c468ae34c02ec3f11
paddy <525854808@qq.com>
2016-08-17 08:33:55 +0800

    上传文件

---
+++ js/js.js
@@ -0,0 +1,18 @@
+/**
+ * Created by babby on 2016/8/16.
+ */
+function fontAuto(width){
+    var winW=document.documentElement.clientWidth;
+    if(winW>=width)
+    {
+        document.documentElement.style.fontSize="625%";
+    }
+    else{
+        document.documentElement.style.fontSize=(winW/width*625)+"%";
+    }
+}
+fontAuto(750);
+
+window.onresize=function(){
+    fontAuto(750);
+};
